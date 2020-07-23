2753bb8b948fd2937aff9c33a1c16ed96bc3cad7
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2020-07-23 18:34:10 +0530

    Inserting a Node Into a Sorted Doubly Linked List

---
+++ Doubly Linked List/insert_sorted_node.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    DoublyLinkedListNode* temp = new DoublyLinkedListNode (data);
+    if(temp->data < head->data){
+        temp->next = head;
+        temp->prev = nullptr;
+        head = temp;
+    }
+    else{
+        DoublyLinkedListNode* p = head, *q;
+        while(temp->data > p->data && p->next != nullptr){
+            q = p;
+            p = p->next;
+        }
+        if(temp->data > p->data){
+            p->next = temp;
+            temp->prev = p;
+            temp->next = nullptr;
+        }
+        else{
+            temp->prev = q;
+            q->next = temp;
+            temp->next = p;
+            p->prev = temp;
+        }
+        
+    }
+    return head;
+}
+
+int main()
