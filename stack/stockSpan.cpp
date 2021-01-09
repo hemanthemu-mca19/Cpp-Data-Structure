ae852cd57def92ab595b9bc8399d77eb97ae881a
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-01-09 16:19:13 +0530

    Create stockSpan.cpp
    Problem Link: https://practice.geeksforgeeks.org/problems/stock-span-problem-1587115621/1/?category[]=Stack&page=1&query=category[]Stackpage1

---
+++ stack/stockSpan.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to calculate span
+// price[]: price array in input
+// n: size of array
+
+
+
+
+vector <int> calculateSpan(int price[], int n)
+{
+   // Your code here
+   vector<int> span(n, 1);
+   stack<int> st;
+   st.push(0);
+   for(int i=1; i<n; i++){
+      
+      while(!st.empty() and price[st.top()] <= price[i]){
+          st.pop();
+      }
+      if(!st.empty())   span[i] = i - st.top();
+      else span[i] = i+1;
+      
+      st.push(i);
+   }
+   
+   return span;
+}
+
+
+// { Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		
+		vector <int> s = calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
