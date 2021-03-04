35568b160c85c19ce16f3fb088bb23681c585f45
Hemant Kumar <55533441+hmnt007@users.noreply.github.com>
2021-03-04 18:12:43 +0530

    Update friendsPairing.cpp

--- Dynamic Programming/friendsPairing.cpp
+++ Dynamic Programming/friendsPairing.cpp
@@ -33,6 +33,22 @@ Sample Output: 4
 		return dp[n];
 		
 	}
+	//TopDown Approach
+	long long countFriendsPairings(int n){
+	long long dp[n+1];
+
+	// Filling dp[] in bottom-up manner using
+	// recursive formula explained above.
+	for (int i=0; i<=n; i++)
+	{
+		if (i <= 2)
+		dp[i] = i;
+		else
+		dp[i] = dp[i-1] + (i-1) * dp[i-2];
+	}
+
+	return dp[n];
+}
 	int main() {
 		int t;
 		cin >>t ;
